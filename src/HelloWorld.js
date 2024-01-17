d3fda144b5d9508ecd108b134fb220108867c96c
rashinkhan-bidchat <rashin.khan@bidchat.com>
2024-01-17 12:16:31 +0530

    changed code for reducing code and fixed some UI

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { imagesSection, navBarMenu } from './constants/ConstantsToUse';
 import './navbar.css';
 
 const HelloWorld = () => {
@@ -8,21 +9,14 @@ const HelloWorld = () => {
       <div
         className="navigation-menu">
         <ul>
-          <li>
-            <a href="/home">About</a>
-          </li>
-          <li>
-            <a href="/about">News</a>
-          </li>
-          <li>
-            <a href="/contact">Services</a>
-          </li>
-          <li>
-            <a href="/contact">Our Team</a>
-          </li>
-          <li>
-            <a href="/contact">Make Enquiry</a>
-          </li>
+          {
+            navBarMenu.length ? 
+            navBarMenu.map((items, index) => (
+              <li key={index}>
+                {items.name}
+              </li>
+            )) : null
+          }
         </ul>
       </div>
     </nav>
@@ -37,27 +31,23 @@ const HelloWorld = () => {
         <br/>Ut enmim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
       </div>
       <div className='container'>
-        <div className="image">
-          <img 
-            src="https://media.istockphoto.com/id/1434218035/photo/austin-suburban-aerial.jpg?s=2048x2048&w=is&k=20&c=T5UkskuSf2Ukm3h6aExidkZruaotvn54-Q6QPwIC7Po="
-            height="700px"
-            width="70%"
-          />
-        </div>
-        <div className="image">
-          <img 
-            src="https://media.istockphoto.com/id/1434218035/photo/austin-suburban-aerial.jpg?s=2048x2048&w=is&k=20&c=T5UkskuSf2Ukm3h6aExidkZruaotvn54-Q6QPwIC7Po="
-            height="700px"
-            width="70%"
-          />
-        </div>
-        <div className="image">
-          <img 
-            src="https://media.istockphoto.com/id/1434218035/photo/austin-suburban-aerial.jpg?s=2048x2048&w=is&k=20&c=T5UkskuSf2Ukm3h6aExidkZruaotvn54-Q6QPwIC7Po="
-            height="700px"
-            width="70%"
-          />
-        </div>
+          {
+            imagesSection.length ? 
+              imagesSection.map((items, index) => (
+                <div className={items.class} key={index}>
+                  <img 
+                    src={items.url}
+                    alt={items.alt}
+                    height="700px"
+                    width="70%"
+                  />
+                </div>
+              )) : null
+          }
+      </div>
+      <div className='bottom-center'>
+        <h1>Client 1</h1>
+        Dubai, United Arab Emirates
       </div>
     </>
   );
---
+++ src/constants/ConstantsToUse.js
@@ -0,0 +1,37 @@
+export const navBarMenu = [
+    {
+        name: 'About'
+    },
+    {
+        name: 'News'
+    },
+    {
+        name: 'Services'
+    },
+    {
+        name: 'Our Team'
+    },
+    {
+        name: 'Make Enquiry'
+    }
+
+]
+
+export const imagesSection = [
+    {
+        url: 'https://media.istockphoto.com/id/1434218035/photo/austin-suburban-aerial.jpg?s=2048x2048&w=is&k=20&c=T5UkskuSf2Ukm3h6aExidkZruaotvn54-Q6QPwIC7Po=',
+        class: 'image-right',
+        alt: 'Right Tilted Image'
+    },
+    {
+        url: 'https://media.istockphoto.com/id/1434218035/photo/austin-suburban-aerial.jpg?s=2048x2048&w=is&k=20&c=T5UkskuSf2Ukm3h6aExidkZruaotvn54-Q6QPwIC7Po=',
+        class: 'image',
+        alt: 'Center Image'
+    },
+    {
+        url: 'https://media.istockphoto.com/id/1434218035/photo/austin-suburban-aerial.jpg?s=2048x2048&w=is&k=20&c=T5UkskuSf2Ukm3h6aExidkZruaotvn54-Q6QPwIC7Po=',
+        class: 'image-rotate',
+        alt: 'Left Tilted Image'
+    }
+
+]
